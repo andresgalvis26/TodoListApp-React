12c122f684f4bd880c4dd55c50492eb5ec8741ed
Andres Galvis <andres.felipe99@hotmail.com>
2023-07-03 18:55:07 -0500

    TodoListApp finalizada.

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export const EditTodoForm = () => {
+export const EditTodoForm = ({editTodo, task}) => {
+
+    // Capturar cambios de estado en el input
+    const [value, setValue] = useState(task.task)
+
+    // Capturar los valores cuando ocurre el evento onSubmit
+    // Y enviarlos por medio de la función 'addToDo'
+    // Esta función esta definida desde el comp padre y se pasa por props.
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        editTodo(value, task.id)
+
+        setValue("")
+        
+    }
+
     return (
-        <div>EditTodoForm</div>
+        <form className='TodoForm' onSubmit={handleSubmit}>
+            <input type="text" className='todo-input' value={value} placeholder="Update Task" onChange={(e) => setValue(e.target.value)} />
+            <button type="submit" className='todo-btn'>Update Task</button>
+        </form>
     )
 }
